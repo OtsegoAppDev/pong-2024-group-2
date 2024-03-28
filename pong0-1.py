7727851b02de73fd74046566902cc963147c4eb4
qlashaway27 <161366366+qlashaway27@users.noreply.github.com>
2024-03-28 09:55:15 -0400

    game for group 2

--- pong0-1.py
+++ pong0-1.py
@@ -1,52 +1,76 @@
 import pygame as pygame
 
-def drawCenterLine():
-    global screenWidth, screenHeight
-    pygame.draw.line(window, white, (screenWidth//2, 0), (screenWidth//2, screenHeight))
-
-def drawScore(font):
-    global scoreA, scoreB
-    
-    text=font.render(str(scoreA), True, white)
-    window.blit(text, (200, 30))
-    text = font.render(str(scoreB), True, white)
-    window.blit(text, (700, 30))
-
 def MoveBall():
-    global scoreA, scoreB, ballSpeedx, ballSpeedy, ballLocation, ball
+    global ballSpeedx, ballSpeedy, ballLocation, ball, scoreA,scoreB
     
     if ballLocation[0] > screenWidth:
         ballSpeedx = -ballSpeedx
         scoreA = scoreA + 1
-    
-    if ballLocation[0] < 0:
+    if ballLocation[0] <= 0:
         ballSpeedx = -ballSpeedx
         scoreB = scoreB + 1
     
+    if ballLocation[0]< 0:
+        ballSpeedx = -ballSpeedx
+        
     if ballLocation[1] < 0:
         ballSpeedy = -ballSpeedy
     
-    if ballLocation[1] > screenHeight:
+    if ballLocation [1] > screenHeight:
         ballSpeedy = -ballSpeedy
+            
         
     ballLocation[0] = ballLocation[0] + ballSpeedx
     ballLocation[1] = ballLocation[1] + ballSpeedy
-    ball = pygame.draw.circle(window, white, ballLocation, radius, 0) 
-    
+    ball=pygame.draw.circle(window, hotpink, ballLocation, radius, 0)
+    #print(ball)
+
 def MovePaddle():
-    global PadASpeed, PadA
-    """
-    Moves the paddle up and down but not off the screen
-    """
-    
+    global PadASpeed, PadA, PadB, PadBSpeed, ball
     if PadA.top <= 0:
-        print("Top of Screen")
+        print("top of screen")
         PadA = PadA.move(0,2)
-        PadASpeed = 0
-    #Add Bottom of Paddle Code here
+        PadASpeed=0
+    if PadB.top <= 0:
+        print("top of screen")
+        PadB = PadB.move(0,2)
+        PadBSpeed=0
+    PadA = PadA.move(0,PadASpeed)
+    PadB = PadB.move(0,PadBSpeed)
+    pygame.draw.rect(window, pinkpurple, PadA)
+    pygame.draw.rect(window, pinkpurple, PadB)
+def drawCenterLine():
+    global screenWidth, screenHeight
+    pygame.draw.line(window,cyan,(screenWidth//2,0),(screenWidth//2,screenHeight))
+
+
+def drawScore(font):
+    global scoreA, scoreB
+    
+    text = font.render(str(scoreA),True, blue)
+    window.blit(text,(200,30))
+    text = font.render(str(scoreB),True,blue)
+    window.blit(text,(700,30))
+    
+def Win(font):
+    global scoreA, scoreB
+    if scoreA >=20:
+        text = font.render(("PlayerAwins"),True, blue)
+        window.blit(text,(500,30))
+
+    if scoreB >=20:
+        text = font.render(("PlayerBwins"),True, blue)
+        window.blit(text,(500,30))
         
-    PadA = PadA.move(0, PadASpeed)
-    pygame.draw.rect(window, white, PadA)
+def Ballchangesize(size):
+    global radius
+    radius = size
+    
+def ScreenWidthsize(sizeW, sizeH):
+    global screenWidth, screenHeight 
+    screenWidth = sizeW
+    screenHeight = sizeH
+    pygame.display.set_mode((sizeW, sizeH))
 
 timer = pygame.time.Clock()
 
@@ -54,23 +78,29 @@ screenWidth = 1000
 screenHeight = 600
 
 window = pygame.display.set_mode([screenWidth, screenHeight])
-
 ballSpeedx = 1
 ballSpeedy = -1
 black = (0,0,0)
-white = (255, 255, 255)
+white = (255,255,255)
+blue = (0,0,255)
+cyan = (66, 214, 142)
+pinkpurple = (214, 34, 214)
+hotpink = (214,34,85)
 radius = 20
-ballLocation=[500, 300]
+ballLocation=[500,300]
 ball = pygame.Rect(ballLocation, (radius, radius))
 
-PadA = pygame.Rect((0,150), (50,300))
+PadA = pygame.Rect((0,150),(10,300))
 PadASpeed = 0
+PadB = pygame.Rect((990,150),(10,300))
+PadBSpeed = 0
 
 scoreA=0
 scoreB=0
 
 pygame.font.init()
-font = pygame.font.Font(None,24)
+print (pygame.font.get_fonts())
+font=pygame.font.SysFont("cambria", 24)
 
 while True:
     for event in pygame.event.get():
@@ -79,20 +109,56 @@ while True:
                 PadASpeed = -2
             if event.key == pygame.K_DOWN:
                 PadASpeed = 2
+            if event.key == pygame.K_w:
+                PadBSpeed = -2
+            if event.key == pygame.K_s:
+                PadBSpeed = 2
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
                 PadASpeed = 0
             if event.key == pygame.K_DOWN:
                 PadASpeed = 0
+            if event.key == pygame.K_w:
+                PadBSpeed = 0
+            if event.key == pygame.K_s:
+                PadBSpeed = 0
+            if event.key == pygame.K_a:
+                radius = 30
+            if event.key == pygame.K_z:
+                radius = 10
+            if event.key == pygame.K_x:
+                radius = 20
+            if event.key == pygame.K_e:
+                sizeW = 2000
+                sizeH = 900
+                ScreenWidthsize(sizeW, sizeH)
+                
+            if event.key == pygame.K_r:
+                sizeW = 900
+                sizeH = 750
+                print("r")
+                ScreenWidthsize(sizeW, sizeH)
+            
+            
+                                
     if PadA.colliderect(ball):
         ballSpeedx = -ballSpeedx
+        ballSpeedx = ballSpeedx +1
+        ballSpeedy = ballSpeedy +1
+    if PadB.colliderect(ball):
+        ballSpeedx = -ballSpeedx
+        ballSpeedx = ballSpeedx +0.5
+        ballSpeedy = ballSpeedy +0.5
+        #print(hit)
+    
     timer.tick(60)
-    window.fill(black)
+    window.fill(black) 
     MoveBall()
     MovePaddle()
     drawCenterLine()
     drawScore(font)
-    pygame.display.flip()
+    Win(font)
     
+    pygame.display.flip()
     #check quit event
     #check up, down, spacebar event
